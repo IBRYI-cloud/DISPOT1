7175ddf5b0dd2a26e865688e052a6cb86abf047d
IBRYI-cloud <bryf302@gmail.com>
2021-08-19 12:51:18 -0400

    hola

---
+++ Infom/ProyectoInfogram/app/src/main/java/com/example/infogram/infogram.java
@@ -0,0 +1,15 @@
+package com.example.infogram;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.infom.R;
+
+public class infogram extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infogram);
+    }
+}
